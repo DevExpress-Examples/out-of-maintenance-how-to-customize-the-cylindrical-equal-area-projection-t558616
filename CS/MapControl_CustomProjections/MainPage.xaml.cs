efd8102bf84f814fe0d427e4a77a7343bb7eab5b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapControl_CustomProjections/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.UI.Xaml.Map;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace MapControl_CustomProjections {
+    public sealed partial class MainPage : Page {
+        ProjectionRatio[] projectionRatios = {
+                                                 new ProjectionRatio("Lambert", 3.14),
+                                                 new ProjectionRatio("Behrmann", 2.36),
+                                                 new ProjectionRatio("Trystan Edwards", 2),
+                                                 new ProjectionRatio("Gall-Peters", 1.57),
+                                                 new ProjectionRatio("Balthasart", 1.3),
+                                                 new ProjectionRatio("Default", 1)
+                                             };
+
+        int sizeValue = 512;
+
+        public ProjectionRatio[] ProjectionRatios { get { return projectionRatios; } }
+
+        public MainPage() {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            ListView listView = sender as ListView;
+            if (listView == null) return;
+
+            ((VectorFileLayer)mapControl.Layers[0]).InitialMapSize = new Size(
+                ((ProjectionRatio)listView.SelectedItem).Value * sizeValue, sizeValue
+            );
+        }
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            listView.SelectedIndex = ProjectionRatios.Length - 1;
+        }
+    }
+
+    public class ProjectionRatio {
+        public ProjectionRatio(string name, double value) {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+
+}
---
+++ VB/MapControl_CustomProjections/MainPage.xaml.vb
@@ -0,0 +1,52 @@
+﻿
+Imports DevExpress.UI.Xaml.Map
+
+Partial Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private projRatios() As ProjectionRatio = {
+            New ProjectionRatio("Lambert", 3.14),
+            New ProjectionRatio("Behrmann", 2.36),
+            New ProjectionRatio("Trystan Edwards", 2),
+            New ProjectionRatio("Gall-Peters", 1.57),
+            New ProjectionRatio("Balthasart", 1.3),
+            New ProjectionRatio("Default", 1)
+        }
+
+    Private sizeValue As Integer = 512
+
+    Public ReadOnly Property ProjectionRatios() As ProjectionRatio()
+        Get
+            Return projRatios
+        End Get
+    End Property
+
+    Public Sub New()
+        Me.InitializeComponent()
+        Me.DataContext = Me
+    End Sub
+
+    Private Sub ListView_SelectionChanged(ByVal sender As Object, ByVal e As SelectionChangedEventArgs)
+        Dim listView As ListView = TryCast(sender, ListView)
+        If listView Is Nothing Then
+            Return
+        End If
+
+        CType(mapControl.Layers(0), VectorFileLayer).InitialMapSize = New Size(CType(listView.SelectedItem, ProjectionRatio).Value * sizeValue, sizeValue)
+    End Sub
+
+    Private Sub Page_Loaded(ByVal sender As Object, ByVal e As Windows.UI.Xaml.RoutedEventArgs)
+        listView.SelectedIndex = ProjectionRatios.Length - 1
+    End Sub
+End Class
+
+Public Class ProjectionRatio
+    Public Sub New(ByVal name As String, ByVal value As Double)
+        Me.Name = name
+        Me.Value = value
+    End Sub
+
+    Public Property Name() As String
+    Public Property Value() As Double
+End Class
+
